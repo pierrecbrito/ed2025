5c29bfe6ab51e626ba7073862984842dbf248ba0
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-28 19:58:26 -0300

    reverseText tests

--- __tests__/Lista01.test.js
+++ __tests__/Lista01.test.js
@@ -7,6 +7,24 @@ describe("Q1. Palavra de forma invertida", () => {
     test("Palavra de forma invertida", () => {
         expect(reverseText("ABACAXI")).toBe("IXACABA");
         expect(reverseText("BANANA")).toBe("ANANAB");
+        expect(reverseText("Olá, mundo!")).toBe("!odnum ,álO");
+        expect(reverseText("abc123")).toBe("321cba");
+        expect(reverseText("AbCdEf")).toBe("fEdCbA");
     });
 
+    test("String vazia", () => {
+        expect(reverseText("")).toBe("");
+    });
+
+    test("String com um caractere", () => {
+        expect(reverseText("A")).toBe("A");
+    });
+
+    test("String com caracteres especiais", () => {
+        expect(reverseText("123!@#")).toBe("#@!321");
+    });
+
+    test("String com espaços", () => {
+        expect(reverseText("A B C")).toBe("C B A");
+    });
 });
